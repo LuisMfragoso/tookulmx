44e02782e6ce4a41e25a562ee2ae093d6bec53d0
LuisMfragoso <luisfragoso_2.0@hotmail.it>
2023-07-24 08:29:42 -0600

    landing v1.02

---
+++ vendor/forms/contact.php
@@ -0,0 +1,13 @@
+<?php
+  $receiving_email_address = 'luis.fragoso@tookul.mx';
+
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+  $content = "Name: " . $name . "Surname: " .surname "\nEmail " . $email . "\nSubject " . $subject . "\nMessage " . $message; 
+  mail($receiving_email_address, "Email", $content);
+  header("Location:https://luisfragoso.tookul.mx/#contact");
+
+?>
